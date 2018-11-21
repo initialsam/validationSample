c0edc09a6e7f989017e4c59577131d936dc1536b
Joey Chen <jay.hatelove@gmail.com>
2018-11-22 00:11:57 +0800

    example

---
+++ UnitTestProject4/Medical.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestProject4
+{
+    public class Medical
+    {
+        public Medical(int year, string content)
+        {
+            Year = year;
+            Content = content;
+        }
+
+        public int Year { get; }
+        public string Content { get; }
+
+        public bool IsValid()
+        {
+            var validators = new Dictionary<int, Func<string, bool>>
+            {
+                {106, this.Validator106 },
+                {107, this.Validator107 },
+            };
+
+            return validators.ContainsKey(Year) && validators[Year](Content);
+        }
+
+        private bool Validator107(string content)
+        {
+            var numberList = Enumerable.Range(1, 14).Except(new[] { 2, 12 });
+            var whiteList = numberList.Select(x => x.ToString()).Concat(new string[] { "2a", "2b", "99" });
+            return whiteList.Contains(content);
+        }
+
+        private bool Validator106(string content)
+        {
+            if (int.TryParse(content, out int value))
+            {
+                return (value >= 1 && value <= 11) || value == 99;
+            }
+
+            return false;
+        }
+    }
+}
--- UnitTestProject4/UnitTest1.cs
+++ UnitTestProject4/UnitTest1.cs
@@ -1,14 +1,28 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace UnitTestProject4
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestCase(106, "2", true)]
+        [TestCase(106, "11", true)]
+        [TestCase(106, "0", false)]
+        [TestCase(106, "12", false)]
+        [TestCase(106, "99", true)]
+        [TestCase(107, "2a", true)]
+        [TestCase(107, "2b", true)]
+        [TestCase(107, "14", true)]
+        [TestCase(107, "1", true)]
+        [TestCase(107, "2", false)]
+        [TestCase(107, "12", false)]
+        [TestCase(107, "99", true)]
+        [TestCase(107, "abc", false)]
+        public void valid_list(int year, string content, bool expected)
         {
+            var medical = new Medical(year, content);
+            Assert.AreEqual(expected, medical.IsValid());
         }
     }
 }
